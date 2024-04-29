4ca56aecc9f66d73df2276aedc2cf6f463856a58
Owen Dolan <odolan27@gmail.com>
2024-04-28 23:34:44 -0400

    updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -117,7 +117,7 @@ export default function Index() {
     const [location, setLocation] = useState<string>('')
 
     return (
-      <div style={{width: '100vw', height: '100vh', display: 'flex', overflowY: 'auto', flexDirection: 'column', padding: 20}}>
+      <div style={{width: '100vw', height: '100vh', display: 'flex', overflowY: 'auto', flexDirection: 'column'}}>
         
         {/* main row contents */}
         <div className={styles.hero_content}>
