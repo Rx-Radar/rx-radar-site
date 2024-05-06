af251f0b6dd048f57412091b14c697539a2d2300
Simon-Sandrew <sandrewsimon@gmail.com>
2024-05-06 01:24:08 -0400

    nav bar fix + contact email added

--- components/NavigationBar/NavigationBar.tsx
+++ components/NavigationBar/NavigationBar.tsx
@@ -1,37 +1,23 @@
-import React, { useState, useRef, useEffect } from 'react';
-import styles from './NavigationBar.module.css';
+import React, { useState, useRef, useEffect } from "react";
+import styles from "./NavigationBar.module.css";
 
-import RxRadarLogo from '@/app/images/RxRadarLogo';
-import RxRadarLogoTarget from '@/app/images/RxRadarLogoTarget';
-
+import RxRadarLogo from "@/app/images/RxRadarLogo";
+import RxRadarLogoTarget from "@/app/images/RxRadarLogoTarget";
 
 /**
  * SearchInput element allows for a filtered list
  * @returns Searchable Input
  */
 export const NavigationBar = () => {
+  return (
+    <div className={styles.navigation_bar}>
+      {/* logo */}
+      <RxRadarLogo />
 
-    return (
-        <div className={styles.navigation_bar}>
-            {/* logo */}
-            <RxRadarLogo/>
-
-            {/* default: menu options */}
-            <div className={styles.menu_options}>
-                <p>About</p>
-                <p>Contact</p>
-                <p>Pricing</p>
-                <RxRadarLogoTarget size={40}/>
-            </div>
-
-            {/* mobile only: menu button */}
-            <button className={styles.menu_button}>
-                <svg width="100%" height="100%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M3 12H15M3 6H21M3 18H21" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                </svg>
-            </button>
-
-        </div>
-    );
-
-}
+      {/* default: menu options */}
+      <div className={styles.menu_options}>
+        <RxRadarLogoTarget size={40} />
+      </div>
+    </div>
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,93 +1,127 @@
 // index page
-'use client';
+"use client";
 
-import Link from 'next/link';
-import { useEffect, useRef, useState } from 'react';
-import { useRouter } from 'next/navigation';
-import styles from './page.module.css';
-import axios from 'axios';
+import Link from "next/link";
+import { useEffect, useRef, useState } from "react";
+import { useRouter } from "next/navigation";
+import styles from "./page.module.css";
+import axios from "axios";
 
 // npm inports
-import 'react-international-phone/style.css';
-import ReactInputVerificationCode from 'react-input-verification-code';
-import { ToastContainer, toast, Bounce } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import "react-international-phone/style.css";
+import ReactInputVerificationCode from "react-input-verification-code";
+import { ToastContainer, toast, Bounce } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 // local inports
-import { NavigationBar } from '../../components/NavigationBar/NavigationBar';
-import QuantumLoader from '../../components/LoaderAnimations/QuantumLoader/QuantumLoader';
-import RxRadarLogoBeacon from './images/RxRadarLogoBeacon';
-import { setupRecaptcha, sendSMSVerification, signUserIn} from '../../utils/AuthVerifier';
-import { PrescriptionSearchForm } from '../../components/PrescriptionSearchForm/PrescriptionSearchFormProps';
-import { validatePrescriptionSearch, isValidSearchTime } from '../../utils/ValidatePrescriptionSearch';
+import { NavigationBar } from "../../components/NavigationBar/NavigationBar";
+import QuantumLoader from "../../components/LoaderAnimations/QuantumLoader/QuantumLoader";
+import RxRadarLogoBeacon from "./images/RxRadarLogoBeacon";
+import {
+  setupRecaptcha,
+  sendSMSVerification,
+  signUserIn,
+} from "../../utils/AuthVerifier";
+import { PrescriptionSearchForm } from "../../components/PrescriptionSearchForm/PrescriptionSearchFormProps";
+import {
+  validatePrescriptionSearch,
+  isValidSearchTime,
+} from "../../utils/ValidatePrescriptionSearch";
 
 // import types
-import { PrescriptionSearch } from '../../types/PrescriptionSearch';
-
+import { PrescriptionSearch } from "../../types/PrescriptionSearch";
 
 export default function Index() {
   const navigation = useRouter(); // navigation router
 
-  const [loading, setLoading] = useState<boolean>(false); 
-  const [prescriptionSearch, setPrescriptionSearch] = useState<PrescriptionSearch>();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [prescriptionSearch, setPrescriptionSearch] =
+    useState<PrescriptionSearch>();
 
   // user search process state
-  type SearchState = 'START' | 'VERIFICATION_SENT' | 'SEARCH_STARTED';
-  const [searchState, setSearchState] = useState<SearchState>('START');
-
+  type SearchState = "START" | "VERIFICATION_SENT" | "SEARCH_STARTED";
+  const [searchState, setSearchState] = useState<SearchState>("START");
 
   // on page load setup
   useEffect(() => {
     setupRecaptcha();
   }, []);
 
-
   // triggers medication search
-  const initializeMedicationSearch = (prescriptionSearch: PrescriptionSearch) => {
+  const initializeMedicationSearch = (
+    prescriptionSearch: PrescriptionSearch
+  ) => {
     setLoading(true);
 
     // check if search is within pharmacy hours
     if (!isValidSearchTime()) {
-      toast.error('Try searching during pharmacy hours', { position: "bottom-center", autoClose: 1000, hideProgressBar: true, pauseOnHover: true, progress: undefined, theme: "light", transition: Bounce }); 
-      return
+      toast.error("Try searching during pharmacy hours", {
+        position: "bottom-center",
+        autoClose: 1000,
+        hideProgressBar: true,
+        pauseOnHover: true,
+        progress: undefined,
+        theme: "light",
+        transition: Bounce,
+      });
+      return;
     }
 
-    const { success, error, newPrescriptionSearch} = validatePrescriptionSearch(prescriptionSearch);
+    const { success, error, newPrescriptionSearch } =
+      validatePrescriptionSearch(prescriptionSearch);
     if (!success) {
       // user filled some part of the form wrong
-      toast.error(error, { position: "bottom-center", autoClose: 1000, hideProgressBar: true, pauseOnHover: true, progress: undefined, theme: "light", transition: Bounce }); 
-      return
+      toast.error(error, {
+        position: "bottom-center",
+        autoClose: 1000,
+        hideProgressBar: true,
+        pauseOnHover: true,
+        progress: undefined,
+        theme: "light",
+        transition: Bounce,
+      });
+      return;
     }
 
     sendSMSVerification(prescriptionSearch.phoneNumber)
-        .then((successMessage) => {
-          setSearchState('VERIFICATION_SENT');
-          setLoading(false);
-        })
-        .catch((error) => console.error('Error sending SMS verification:', error));
+      .then((successMessage) => {
+        setSearchState("VERIFICATION_SENT");
+        setLoading(false);
+      })
+      .catch((error) =>
+        console.error("Error sending SMS verification:", error)
+      );
 
     // set local prescriptionSearch
     setPrescriptionSearch(newPrescriptionSearch);
-  }
+  };
 
   // verifies sms auth on user enter code completion
   const verifyCodeEntered = (code: string) => {
     signUserIn(code)
-        .then((userSessionToken) => {
-          setSearchState('SEARCH_STARTED');
-          makeInitSearchPost(userSessionToken)
-        })
-        .catch((error) => console.log(error));
-  }
+      .then((userSessionToken) => {
+        setSearchState("SEARCH_STARTED");
+        makeInitSearchPost(userSessionToken);
+      })
+      .catch((error) => console.log(error));
+  };
 
   // calls init-search-bland endpoint to initiate prescription search
   async function makeInitSearchPost(userSessionToken: string) {
+    const url =
+      "https://us-central1-rxradar.cloudfunctions.net/init-search-bland-cloudtask"; // calls init-search-bland [[MVP]]
 
-    const url =  'https://us-central1-rxradar.cloudfunctions.net/init-search-bland-cloudtask'; // calls init-search-bland [[MVP]]
-    
     if (!prescriptionSearch) {
-      toast.error('woah, somethings not working. Try searching again', { position: "bottom-center", autoClose: 1000, hideProgressBar: true, pauseOnHover: true, progress: undefined, theme: "light", transition: Bounce }); 
-      return
+      toast.error("woah, somethings not working. Try searching again", {
+        position: "bottom-center",
+        autoClose: 1000,
+        hideProgressBar: true,
+        pauseOnHover: true,
+        progress: undefined,
+        theme: "light",
+        transition: Bounce,
+      });
+      return;
     }
 
     const body = {
@@ -99,96 +133,174 @@ export default function Index() {
         dosage: prescriptionSearch.prescription.dosage,
         brand_or_generic: prescriptionSearch.prescription.brand,
         quantity: prescriptionSearch.prescription.quantity,
-        type: prescriptionSearch.prescription.type
-      }
+        type: prescriptionSearch.prescription.type,
+      },
     };
-  
+
     try {
       const response = await axios.post(url, body, {
         headers: {
-          'Content-Type': 'application/json',
-        }
+          "Content-Type": "application/json",
+        },
       });
 
-      console.log('Response:', response.data);
-      console.log("Status:", response.status)
+      console.log("Response:", response.data);
+      console.log("Status:", response.status);
       return response.data;
     } catch (error) {
-      console.error('Error:', error);
-      toast.error('whoah, somethings not working. Try searching again', { position: "bottom-center", autoClose: 1000, hideProgressBar: true, pauseOnHover: true, progress: undefined, theme: "light", transition: Bounce }); 
+      console.error("Error:", error);
+      toast.error("whoah, somethings not working. Try searching again", {
+        position: "bottom-center",
+        autoClose: 1000,
+        hideProgressBar: true,
+        pauseOnHover: true,
+        progress: undefined,
+        theme: "light",
+        transition: Bounce,
+      });
     }
   }
 
   // main hero content with medication search form
   const HeroContent = () => {
     return (
-      <div style={{width: '100vw', height: '100vh', display: 'flex', overflowY: 'auto', flexDirection: 'column'}}>
+      <div
+        style={{
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+          overflowY: "auto",
+          flexDirection: "column",
+        }}
+      >
         {/* main row contents */}
         <div className={styles.hero_content}>
-
           {/* left box (content) */}
           <div className={styles.hero_text_container}>
-            <p style={{color: '#FB4E00'}}>Meds in shortage?</p>
+            <p style={{ color: "#FB4E00" }}>Meds in shortage?</p>
             <p>We find a nearby pharmacy that has them</p>
-            <p style={{color: '#FB4E00'}}>Then we'll text you in ~10</p>
+            <p style={{ color: "#FB4E00" }}>Then we'll text you in ~10</p>
           </div>
 
           {/* right box (input form)*/}
-          <PrescriptionSearchForm loading={false} initializeMedicationSearch={initializeMedicationSearch}/>
+          <PrescriptionSearchForm
+            loading={false}
+            initializeMedicationSearch={initializeMedicationSearch}
+          />
         </div>
 
         {/* ...rest of page content here */}
       </div>
     );
-  }
+  };
 
   // verification code entry page
   const VerificationContent = () => {
     return (
-      <div style={{ width: '100vw', height: '100vh', backgroundColor: 'white', color: 'black', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
-          <p className={styles.enter_verification_code_text}>Enter SMS verification code to continue</p>
-          
-          <div className={styles.input_verification}>
-            <ReactInputVerificationCode length={6} autoFocus={true} placeholder='' onCompleted={(code) => verifyCodeEntered(code)}/>
-          </div>
+      <div
+        style={{
+          width: "100vw",
+          height: "100vh",
+          backgroundColor: "white",
+          color: "black",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <p className={styles.enter_verification_code_text}>
+          Enter SMS verification code to continue
+        </p>
+
+        <div className={styles.input_verification}>
+          <ReactInputVerificationCode
+            length={6}
+            autoFocus={true}
+            placeholder=""
+            onCompleted={(code) => verifyCodeEntered(code)}
+          />
+        </div>
       </div>
     );
-  }
+  };
 
   // displays search sent page
   const SearchSentContent = () => {
     return (
-      <div style={{ width: '100vw', height: '100vh', backgroundColor: '#FBCEB1', color: 'black', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
+      <div
+        style={{
+          width: "100vw",
+          height: "100vh",
+          backgroundColor: "#FBCEB1",
+          color: "black",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
         <div className={styles.hero_text_container}>
-          <p style={{color: 'black'}}>Searching for meds!</p>
-          <p style={{color: '#F94D00'}}>We'll text you soon</p>
+          <p style={{ color: "black" }}>Searching for meds!</p>
+          <p style={{ color: "#F94D00" }}>We'll text you soon</p>
         </div>
       </div>
     );
-  }
-  
+  };
 
   // loading screen
   const Loader = () => {
-      return <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
-          <QuantumLoader/>
-          <p style={{marginTop: 28}}>Sending Phone Verification</p>
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <QuantumLoader />
+        <p style={{ marginTop: 28 }}>Sending Phone Verification</p>
       </div>
-  }
+    );
+  };
+
+  // add your contact information here
+  const email = "rxradarcontact@gmail.com"; // Set your email address here
+  // Footer component for displaying contact information
+  const FooterBar = () => {
+    return (
+      <div className={styles.footer}>
+        <u>
+          <a href={`mailto:${email}`}>{email}</a>
+        </u>
+      </div>
+    );
+  };
 
   // return main page contents
   return (
-    <div style={{position: 'relative', width: '100vw', height: '100vh', display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center', color: 'black'}}>    
-      <NavigationBar/>
+    <div
+      style={{
+        position: "relative",
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        color: "black",
+      }}
+    >
+      <NavigationBar />
 
-      { searchState == 'START' && <HeroContent/> }
-      { searchState == 'VERIFICATION_SENT' && <VerificationContent/> }
-      { searchState == 'SEARCH_STARTED' && <SearchSentContent/> }
+      {searchState == "START" && <HeroContent />}
+      {searchState == "VERIFICATION_SENT" && <VerificationContent />}
+      {searchState == "SEARCH_STARTED" && <SearchSentContent />}
 
-      <div id='recaptcha'></div>
+      <div id="recaptcha"></div>
       <ToastContainer />
+      <FooterBar />
     </div>
   );
 }
-
-
