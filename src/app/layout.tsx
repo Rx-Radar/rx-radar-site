08c2ad1eac92607125e147a2996c20b5f3b4d4ff
Owen Dolan <odolan27@gmail.com>
2024-05-06 14:26:34 -0400

    added google analytic tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Head from 'next/head'
+import Script from 'next/script'
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,6 +18,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-98YDGN7F36"></Script>
+        <Script id="google-analytics">
+          {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-98YDGN7F36');
+          `}
+        </Script>
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
