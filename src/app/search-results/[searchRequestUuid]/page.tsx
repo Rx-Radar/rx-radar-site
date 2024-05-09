8251dccc9f075ad6fc7de839a427d67d24ff680c
Owen Dolan <odolan27@gmail.com>
2024-05-08 23:49:05 -0400

    added custom urls

---
+++ src/app/search-results/[searchRequestUuid]/page.tsx
@@ -0,0 +1,32 @@
+'use client';
+ 
+import { useRouter, notFound } from 'next/navigation'; 
+import { useEffect } from 'react';
+
+
+export default function SearchResultPage({params}: {
+  params: {
+    searchRequestUuid: string;
+  }
+}) {
+  if (params.searchRequestUuid !== 'test') {
+    return notFound()
+  }
+
+  // useEffect(() => {
+  //   if (params.searchRequestUuid !== 'test') {
+  //     return notFound()
+  //   }
+  // })
+
+  return (
+    <div style={{backgroundColor: 'orange', width: '100%', height: '100%', padding: 20}}>
+      <h1>RxRadar: Prescription search overview</h1>
+      <div style={{backgroundColor: 'white', display: 'flex', flexDirection: 'column', padding: 16, borderRadius: 16, width: 'fit-content'}}>
+        <p>Your search for</p>
+      </div>
+      <p>Dynamic Text based on search_request_uuid: {params.searchRequestUuid}</p>
+    </div>
+  );
+};
+
