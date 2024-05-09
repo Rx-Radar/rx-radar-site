306eabd2b9226a40d8571e75d290f8e17f0bebf6
Owen Dolan <odolan27@gmail.com>
2024-05-09 15:21:24 -0400

    installed firebase tools

--- src/app/search-results/[searchRequestUuid]/page.tsx
+++ src/app/search-results/[searchRequestUuid]/page.tsx
@@ -22,8 +22,13 @@ export default function SearchResultPage({params}: {
   return (
     <div style={{backgroundColor: 'white', width: '100%', height: '100%', padding: 20}}>
       <h1>RxRadar: Prescription search overview</h1>
-      <div style={{backgroundColor: '#FBCEB1', border: '2px solid #F94E02', display: 'flex', flexDirection: 'column', padding: 16, borderRadius: 16, width: 'fit-content'}}>
-        <p>Your search for</p>
+      <div style={{backgroundColor: '#FBCEB1', border: '2px solid #F94E02', display: 'flex', flexDirection: 'column', padding: 16, borderRadius: 16, width: '80%'}}>
+      
+        <p style={{fontSize: 18, width: 'fit-content'}}>Your search for <p style={{fontWeight: '700'}}>Focalin</p> on <p style={{fontWeight: '700'}}>3/18/24</p>:</p>
+        <p>Focalin</p>
+        <p>10mg</p>
+        <p>extended release</p>
+        <p>Focalin</p>
       </div>
       <p>search_request_uuid: {params.searchRequestUuid}</p>
     </div>
