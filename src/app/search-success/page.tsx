99206a2ed977d303b8a140d7ee7072ab6d4df534
Owen Dolan <odolan27@gmail.com>
2024-05-14 11:11:27 -0400

    added stripe listener

--- src/app/search-success/page.tsx
+++ src/app/search-success/page.tsx
@@ -2,7 +2,7 @@
 
 
 
-export const PrescriptionSearchForm = () => {
+export default function SearchSuggestPagw() {
     return <div style={{width: '100%', height: '100%', backgroundColor: 'green', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
         <p style={{fontSize: 27}}> Prescription Search Placed</p>
     </div>
