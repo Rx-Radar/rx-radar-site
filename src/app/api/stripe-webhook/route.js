8699b87acf1c00c9700bb56db2da242a25fecb5e
Owen Dolan <odolan27@gmail.com>
2024-05-13 20:33:15 -0400

    updates

--- src/app/api/stripe-webhook/route.js
+++ src/app/api/stripe-webhook/route.js
@@ -1,13 +1,13 @@
 // stripe webhook handler
 
 import Stripe from 'stripe';
-// import { db } from '../firebase-config';
+import { db } from '../firebase-config';
 
 // move this from here
 STRIPE_SECRET_KEY = 'sk_test_51LAi2vDj2y9TDEKrIYAAmSAQb87yWT0El6kTYypvIoBQZjLNuBRaRf7y3Fu72yr1ql8Z5n93P2EziPcyCR5olsLe005KDjmk5Y'
 
 const stripe = new Stripe(STRIPE_SECRET_KEY, {
-  // Set the API version to use with stripe
+  // Set the API version to use with Stripe
   apiVersion: '2024-04-10',
 });
 
@@ -18,8 +18,7 @@ export const config = {
   },
 };
 
-export async function GET(request, response) {
-
+const stripeWebhookHandler = async (request, response) => {
   console.log('checking this thing', request)
   if (request.method === 'POST') {
 
