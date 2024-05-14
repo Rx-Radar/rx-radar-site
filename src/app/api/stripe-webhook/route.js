74af129a7bf5a5e555c8fabbf50974bf6d7bfa54
Owen Dolan <odolan27@gmail.com>
2024-05-13 22:11:36 -0400

    try

--- src/app/api/stripe-webhook/route.js
+++ src/app/api/stripe-webhook/route.js
@@ -1,17 +1,16 @@
 // stripe webhook handler
 import Stripe from 'stripe';
 
-// move this from here
-const STRIPE_SECRET_KEY = 'sk_test_51LAi2vDj2y9TDEKrIYAAmSAQb87yWT0El6kTYypvIoBQZjLNuBRaRf7y3Fu72yr1ql8Z5n93P2EziPcyCR5olsLe005KDjmk5Y'
-const endpointSecret = 'whsec_pSd2kFFsHa9vNDiGRPYN1RdelGf8ARwr'
-
-const stripe = new Stripe(STRIPE_SECRET_KEY, {
-    // Set the API version to use with Stripe
-    apiVersion: '2024-04-10',
-});
-
 
 export async function POST(request) {
+    // move this from here
+    const STRIPE_SECRET_KEY = 'sk_test_51LAi2vDj2y9TDEKrIYAAmSAQb87yWT0El6kTYypvIoBQZjLNuBRaRf7y3Fu72yr1ql8Z5n93P2EziPcyCR5olsLe005KDjmk5Y'
+    const endpointSecret = 'whsec_pSd2kFFsHa9vNDiGRPYN1RdelGf8ARwr'
+
+    const stripe = new Stripe(STRIPE_SECRET_KEY, {
+        // Set the API version to use with Stripe
+        apiVersion: '2024-04-10',
+    });
 
     if (request.method === 'POST') {
 
