29f8c907feea3fb3cfc5b4eff4298c33d3eb0ae4
Owen Dolan <odolan27@gmail.com>
2024-05-13 20:55:54 -0400

    push

--- src/app/api/stripe-webhook/route.js
+++ src/app/api/stripe-webhook/route.js
@@ -1,10 +1,10 @@
 // stripe webhook handler
-
 import Stripe from 'stripe';
-// import { db } from '../firebase-config';
+import { db } from '../firebase-config';
 
 // move this from here
 STRIPE_SECRET_KEY = 'sk_test_51LAi2vDj2y9TDEKrIYAAmSAQb87yWT0El6kTYypvIoBQZjLNuBRaRf7y3Fu72yr1ql8Z5n93P2EziPcyCR5olsLe005KDjmk5Y'
+
 
 const stripe = new Stripe(STRIPE_SECRET_KEY, {
   // Set the API version to use with Stripe
@@ -19,47 +19,44 @@ export const config = {
 };
 
 const stripeWebhookHandler = async (request, response) => {
-  console.log('checking this thing', request)
-  if (request.method === 'POST') {
+    console.log('checking this thing', request)
+    if (request.method === 'POST') {
 
     if (typeof sig !== 'string') {
-      // Return a 400 Bad Request error if signature is missing or not a string
-      return res.status(400).end('Invalid Stripe Signature');
+        // Return a 400 Bad Request error if signature is missing or not a string
+        return res.status(400).end('Invalid Stripe Signature');
     }
-
+    
     const sig = request.headers['stripe-signature'];
-
     let event;
-
     try {
-      event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+        event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
     } catch (err) {
-      response.status(400).send(`Webhook Error: ${err.message}`);
-      return;
+        response.status(400).send(`Webhook Error: ${err.message}`);
+        return;
     }
-
     // Handle the event based on its type
     switch (event.type) {
-      case 'payment_intent.succeeded':
+    case 'payment_intent.succeeded':
         // Handle payment success event
         console.log('Payment intent succeeded:', event.data.object);
         break;
-      case 'payment_intent.payment_failed':
+    case 'payment_intent.payment_failed':
         // Handle payment failure event
         console.log('Payment intent failed:', event.data.object);
         break;
-      // Add more cases for other event types as needed
-      default:
+    // Add more cases for other event types as needed
+    default:
         console.log(`Unhandled event type: ${event.type}`);
     }
-
     // Return a response to acknowledge receipt of the event
     response.json({ received: true });
-  } else {
-    // Return a 405 Method Not Allowed error for non-POST requests
-    response.setHeader('Allow', 'POST');
-    response.status(405).end('Method Not Allowed');
-  }
+    
+    } else {
+        // Return a 405 Method Not Allowed error for non-POST requests
+        response.setHeader('Allow', 'POST');
+        response.status(405).end('Method Not Allowed');
+    }
 };
 
 export default stripeWebhookHandler;
