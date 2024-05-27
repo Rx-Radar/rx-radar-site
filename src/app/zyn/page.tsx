bc530d650daee82f0a0db2386100ee5b58d1602e
Owen Dolan <odolan27@gmail.com>
2024-05-26 21:32:45 -0400

    setting defined headerbar height

--- src/app/zyn/page.tsx
+++ src/app/zyn/page.tsx
@@ -6,13 +6,13 @@ export default async function ZynRadar() {
   const res = await fetch(`http://rx-radar.com/api/zyn-get-store`, { cache: 'no-store' }); // production
   const data = await res.json();
 
-  return <div className="flex flex-col h-screen overflow-hidden">
+  return <div className="flex flex-col h-screen">
     <Link href="/" className=" w-full py-1 bg-[#F94D00] text-white text-sm font-semibol flex justify-center">
       Can't find your ADHD meds? Click here
     </Link>
 
     {/* header bar */}
-    <div className="w-full, p-2 bg-[#f9f4f4] flex flex-wrap justify-between items-center">
+    <div className="w-full, p-2 bg-[#f9f4f4] flex flex-wrap justify-between items-center h-12">
       <img
       style={{width: 150, height: 31}}
       src="/zynlogo.png"
