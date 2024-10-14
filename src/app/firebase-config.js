d2386927924e1991d5145a09d66336c90033a520
Owen Dolan <41007430+odolan@users.noreply.github.com>
2024-10-13 22:25:22 -0400

    Update firebase-config.js

--- src/app/firebase-config.js
+++ src/app/firebase-config.js
@@ -8,11 +8,11 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDxwmfjF7IGF4z5vxUH1quzK7W9lYjJTzE",
-  authDomain: "rxradar.firebaseapp.com",
+  apiKey: "********HIDDEN********",
+  authDomain: "********HIDDEN********",
   projectId: "rxradar",
   storageBucket: "rxradar.appspot.com",
-  messagingSenderId: "290779208390",
+  messagingSenderId: "********HIDDEN********",
   appId: "1:290779208390:web:f0898a529b6b22dda0d5c7",
   measurementId: "G-ZFCZQJ37KK"
 };
